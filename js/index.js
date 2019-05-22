70f899eb996d756ef7179dec4873d2544f4faef4
wanfarhan <wanfarhannajmi@gmail.com>
2019-05-22 12:16:02 +0800

    changes formula
    change this line to  var distance = countDownDate + now; instead of   var distance = countDownDate - now; see if this can works

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 17, 2019 21:00:00").getTime();
+var countDownDate = new Date("May 21, 2019 06:48:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -8,7 +8,7 @@ var x = setInterval(function() {
   var now = new Date().getTime();
 
   // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+  var distance = countDownDate + now;
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
